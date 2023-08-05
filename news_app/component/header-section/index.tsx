d3ed94b4cbc0e9a60d0898f4b5b59e6bdadeace9
Anurag1096 <anuragqwerty09@outlook.com>
2023-08-05 19:12:36 +0530

    wip: setting up the home section

---
+++ news_app/component/header-section/index.tsx
@@ -0,0 +1,17 @@
+
+const HeaderSection=()=>{
+    return(<>
+        <div>
+            <div id="section-1">
+                <div id="top-img"></div>
+                <div id="bellow-top">
+                    <div id="tagline"></div>
+                    <div id="more-info"></div>
+                </div>
+            </div>
+            <div id="section-2">
+                news list
+            </div>
+        </div>
+        </>)
+}
