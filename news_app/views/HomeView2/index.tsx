496b3d16d6134ff76265a71b7ad9a0491e39837e
Anurag1096 <anuragqwerty09@outlook.com>
2023-08-14 18:24:44 +0530

    wip:adding more content to fylo landing page

--- news_app/views/HomeView2/index.tsx
+++ news_app/views/HomeView2/index.tsx
@@ -19,6 +19,15 @@ const HomeView2 = () => {
             <img src={`/fylo/illustration-intro.png`} alt="intro" />
           </div>
         </div>
+        <div className="flex flex-col justify-center items-center">
+          
+            <h1 className="text-4xl my-4 ">All your files in one secure location,  </h1>
+            <h1 className="text-4xl mb-4">accessible anywhere.</h1>
+          
+          <p className="text-sm text-gray-400">Fylo stroes your most important files in one secure location.</p>
+          <p className="text-sm text-gray-400">Access them wherever you need, share and collaborate with friends and family, and co-workers.</p>
+          <button className="my-8 bg-blue-300 text-xl py-4 px-4 rounded-2xl border-2 transition-colors transiton  hover:bg-pink-900 hover:text-2xl"> Get Started</button>
+        </div>
       </main>
     </>
   );
